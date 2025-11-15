31b1f71fdc0135b77cb27395d5b0b81b45729453
Ankit Virdi <48716928+ankitvirdi4@users.noreply.github.com>
2025-11-15 16:56:32 +0000

    updated corsorigins

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title=settings.app_name, version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins="https://warantix-app.vercel.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
