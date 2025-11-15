b3a17dd912ab05d08c28cfec90bc911e4f0fa0a2
Ankit Virdi <48716928+ankitvirdi4@users.noreply.github.com>
2025-11-15 17:02:51 +0000

    CORS append

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title=settings.app_name, version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://warantix-app.vercel.app",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
